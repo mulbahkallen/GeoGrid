302b295ea5eb182a6415df9461c82045de63794d
Mulbah Kallen <37091806+mulbahkallen@users.noreply.github.com>
2025-04-18 12:16:44 -0500

    Update streamlit_geo_grid_tracker.py

--- streamlit_geo_grid_tracker.py
+++ streamlit_geo_grid_tracker.py
@@ -31,7 +31,7 @@ def _build_colormap():
 
 class GeoGridTracker:
     def __init__(self, google_maps_api_key: str, scraperapi_key: str):
-        self.gmaps_client = googlemaps.Client(key=AIzaSyDRIJdgLLpsKERUJiGdLqkATCr9oEeokAI)
+        self.gmaps_client = googlemaps.Client(key="AIzaSyDRIJdgLLpsKERUJiGdLqkATCr9oEeokAI")
         self.scraperapi_key = "f3e829e606f9b61188f69d52987999c2"
         self.results_data = []
 
